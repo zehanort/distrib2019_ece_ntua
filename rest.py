985686fe72cb5c402d50e2bc70dfd4e4e6da5971
Sotiris Niarchos <sot.niarchos@gmail.com>
2019-03-14 02:15:31 +0200

    fixed get_ring

--- rest.py
+++ rest.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
 
         @app.route(cfg.GET_RING, methods=['POST'])
         def get_ring():
-            node.ring = request.form['ring']
+            node.ring = [(k, v) for k, v in request.form.items()]
             return 'node {} received ring'.format(node.node_id), 200
 
         app.run(host=address, port=port, threaded=True)
