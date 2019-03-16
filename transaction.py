6b39d48b4bcd87a1986c862d0d886919aa1eb2ca
nicklie <nikpatris@gmail.com>
2019-03-16 21:38:46 +0200

    fix transcations constructor

--- transaction.py
+++ transaction.py
@@ -12,9 +12,9 @@ class Transaction(Utilizable):
         # args: inputs, sender_address, recipient_address, amount
         self.__dict__.update(data)
 
-        if not isinstance(self.inputs, UtilizableList):
-            print("I am here")
-            self.inputs = UtilizableList([TransactionOuput(**i) for i in self.inputs])
+        # if not isinstance(self.inputs, UtilizableList):
+        #     print("I am here")
+        #     self.inputs = UtilizableList([TransactionOuput(**i) for i in self.inputs])
 
         self.transaction_id = self.hash()
 
