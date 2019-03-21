16b7a9598fbd3a64b6f77b2f6d5dd72b52463c7a
Sotiris Niarchos <sot.niarchos@gmail.com>
2019-03-21 05:38:11 +0200

    debugged network termination

--- code/cfg.py
+++ code/cfg.py
@@ -48,10 +48,10 @@ BLOCK_TIME = '/stats/blocktime'
 # needed by the site to initialize network
 
 addresses = [
-    '192.168.0.2'
-    '192.168.0.3'
-    '192.168.0.4'
-    '192.168.0.5'
+    '192.168.0.2',
+    '192.168.0.3',
+    '192.168.0.4',
+    '192.168.0.5',
     '192.168.0.6'
 ]
 start_port = 5000
